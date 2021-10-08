21dd628eb2976a61780295b94434dacc20690add
Simone Scarduzio <simone@readonlyrest.com>
2021-10-08 18:52:04 +0200

    change

--- express/server.js
+++ express/server.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
 const db = require('./_helpers/db')
-
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(cookieParser());
+
 router.get('/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Hello from >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>></h1>');
@@ -18,6 +22,9 @@ router.use('/accounts', db.initialize, require('./auth/accounts/accounts.control
 router.get('/another', (req, res) => res.json({route: req.originalUrl}));
 router.post('/', (req, res) => res.json({postBody: req.body}));
 
+
+const app = express();
+app.use(cors({origin: (origin, callback) => callback(null, true), credentials: true}));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => {
